8198ba2da4f2598fa1b30051f751266c5bb4370f
RealPillCosby <ograsmo@gmail.com>
2017-03-16 13:25:04 +0100

    Single file
    Added single.php for ordinary article use.

--- single.php
+++ single.php
@@ -1,39 +1,11 @@
-<?php 
-
-get_header();
-
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-
-    <article class="post">
-        
-        <?php the_post_thumbnail('large-thumbnail'); ?>
-        
-        <p class="post-info"><?php the_category( ' ' ); ?> <?php the_time('d M, Y'); ?></p>
-        
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        
-        <?php the_content(); ?>
-        
-    </article>
-
-    <?php endwhile;
-        
-    else :
-        echo '<p>No content found</p>';
-    
-    endif; ?>
-
-<div class="relevantarticles">
-
-    <div id="r-artikler-tekst">Flere Relevante Artikler</div>
-
-    <?php if ( function_exists( "get_yuzo_related_posts" ) ) { get_yuzo_related_posts(); } ?>
-    
-</div>
-
 <?php
 
-get_footer();
+get_header(); ?>
 
-?>
+
+    
+<?php get_template_part( 'content', 'single'); ?>
+        
+  
+
+<?php get_footer(); ?>
