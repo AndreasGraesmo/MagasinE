d4e56b3311920de362d970f39b9a6f3092ece4d8
kevin0405 <kevin.danielsen@hotmail.com>
2017-05-15 13:38:38 +0200

    Excerpt brackets fix
    Fikset brackets excerpten

--- index.php
+++ index.php
@@ -23,9 +23,10 @@ if (have_posts()) :
 
              <?php if(!empty($post->post_excerpt)) {
      //This post have an excerpt, let's display it
-    the_excerpt();
+    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>"id="excerpt">[...]</a><?php
  } else {
-     echo  custom_field_excerpt();
+    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>" id="excerpt">[...]</a><?php
+     
  } ?>  
 
 
@@ -52,9 +53,9 @@ if (have_posts()) :
 
              <?php if(!empty($post->post_excerpt)) {
      //This post have an excerpt, let's display it
-    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>">[...]</a><?php
+    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>"id="excerpt">[...]</a><?php
  } else {
-    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>">[...]</a><?php
+    echo  custom_field_excerpt(); ?><a href="<?php the_permalink();?>"id="excerpt">[...]</a><?php
      
  } ?> 
 
